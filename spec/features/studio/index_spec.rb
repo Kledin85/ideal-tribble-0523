8ba3c2f72122d624fc194704285cc4b7e5c59743
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-01-02 10:37:43 -0600

    Have the studio index page displaying name and location

--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'The studios index page' do
 # And under each studio I see all of the studio's movies
 # including the movie's title, creation year, and genre
   it 'displays all the studios name and location' do
-    @studio_1 = Studio.create!("Universal Studios", "Hollywood")
-    @studio_2 = Studio.create!("Pixar", "Hollywood")
+    @studio_1 = Studio.create!([{name: "Universal Studios"}, { location: "Hollywood"}])
+    @studio_2 = Studio.create!([{name: "Pixar"}, {location: "Hollywood"}])
     visit '/studios'
     
-    expect(page).to have_content("Universal studios")
+    expect(page).to have_content("Universal Studios")
     expect(page).to have_content("Pixar")
+    expect(page).to have_content("Hollywood")
   end
 end
